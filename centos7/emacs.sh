3668ce83b62b4a312afd4fd3ed07f1fa143daee0
Ray Chen <rc.hyena@gmail.com>
2017-10-22 13:22:11 -0400

    Update emacs setup script after testing on Urchin.

--- centos7/emacs.sh
+++ centos7/emacs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-set -e
-
-yum -y install \
-    emacs \
-    git
+if ! (rpm -qa | grep ^emacs && rpm -qa | grep ^git) > /dev/null; then
+    sudo yum -y install \
+	emacs \
+	git
+fi
 
 BASEDIR=~/.local/packages
 
 mkdir -p $BASEDIR
-pushd $BASEDIR
+pushd $BASEDIR > /dev/null
+
+echo "Installing Emacs Solarized color scheme"
 git clone https://github.com/sellout/emacs-color-theme-solarized
+
+echo "Installing Emacs Chapel mode"
 git clone https://github.com/russel/Emacs-Chapel-Mode
-popd
+
+popd > /dev/null
 
 cp ../.emacs ~/
